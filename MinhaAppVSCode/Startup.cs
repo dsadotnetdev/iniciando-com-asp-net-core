18a75a655b30b54e814a8334937e1e06852c2fd9
Daniel Sena <dsawebdev@hotmail.com>
2022-07-11 19:42:48 -0300

    Adiciona exemplo da aula "Startup.cs e Middlewares"

--- MinhaAppVSCode/Startup.cs
+++ MinhaAppVSCode/Startup.cs
@@ -21,12 +21,10 @@ namespace MinhaAppVSCode
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
@@ -35,7 +33,6 @@ namespace MinhaAppVSCode
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -45,13 +42,14 @@ namespace MinhaAppVSCode
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            app.UseMeuMiddleware();
 
             app.UseMvc(routes =>
             {
